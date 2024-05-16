2cda4886b7e3241ecd39dc44f521a9a7ae871c4d
Nhat0924 <90586536+Nhat0924@users.noreply.github.com>
2024-05-16 15:56:44 -0500

    Add files via upload

---
+++ Term Project/src/index.php
@@ -0,0 +1,23 @@
+<!-- HTML Interface -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="styles.css">
+    <title>COMP 5120 - Term Project</title>
+</head>
+<body>
+    <h1>COMP 5120 - Term Project</h1>
+    <h2>Nhat Nguyen - nhn0001@auburn.edu</h2>
+    <hr class="separator">
+    <p>Query Tables </p>
+    <form method="post">
+    <textarea name="sqlQuery" placeholder="Enter your query here" class="query-box"></textarea>
+    <input type="submit" name="submitted" value="Submit">
+    <input type="reset" name = "clear" value="Clear">
+</form>
+    <!-- Retrieve query results and output from query.php -->
+    <?php
+        include_once 'query.php';
+    ?>
+</body>
+</html>
